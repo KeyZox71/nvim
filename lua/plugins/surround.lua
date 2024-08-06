9cf2456017252d8b2ed9f626ce554339d3645255
adjoly <adamjly@proton.me>
2024-08-06 22:31:02 +0200

    「🏗️」 wip: testing things, might broke.

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,7 +1,11 @@
 return ({
 	"kylechui/nvim-surround",
+<<<<<<< HEAD
 	version = "*", -- For stability; omit to use `main` branch for latest features
 	event = "VeryLazy",
+=======
+	version = "*",     -- For stability; omit to use `main` branch for latest features
+>>>>>>> 15157b2 (「🏗️」 wip: testing things, might broke.)
 	config = function()
 		require("nvim-surround").setup({})
 	end
