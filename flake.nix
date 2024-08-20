9463078b9c6b590a967e88141b3abbd8d1aa30e6
adjoly <adamjly@proton.me>
2024-08-20 20:03:12 +0200

    「🔨」 fix: Change flake to a home manager module

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-	description = "My neovim config as a flake !";
+  description = "My Neovim config as a flake!";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -15,18 +15,21 @@
       pkgs = import nixpkgs { inherit system; };
     in
     {
-      # Define the Home Manager module
+      # Define the overlay for custom modules
       overlays = [
         (final: prev: {
-          nvimzoxModule = { config, lib, pkgs, ... }: with lib; {
+          myNeovimModule = { config, lib, ... }: with lib; {
             options.programs.neovim.nvimzox.enable = mkOption {
               type = types.bool;
               default = false;
               description = "Enable the nvimzox configuration for Neovim.";
             };
+
             config = mkIf config.programs.neovim.nvimzox.enable {
-				programs.neovim.enable = true;
-				xdg.configFile.neovim.source = ./;
+              programs.neovim.enable = true;
+
+              home.file.".config/nvim/init.lua".source = ./init.lua;
+              home.file.".config/nvim/lua".source = ./lua;
             };
           };
         })
