026b5138dbb6a0320ba706bc850abc735f574435
Adam Joly <adjoly@student.42angouleme.fr>
2024-10-06 16:25:02 +0200

    「📝」 doc: Added some doc to wastebin

--- lua/plugins/wastebin.lua
+++ lua/plugins/wastebin.lua
@@ -4,7 +4,10 @@ return {
     config = function ()
 		require("wastebin").setup({
 			url = "https://wastebin.vaultzox.duckdns.org",
-			open_cmd = "wl-copy",
+			-- To open on default browser
+			open_cmd = "open",
+			-- To copy on hyprland
+			-- open_cmd = "wl-copy",
 		})
 	end,
   }
