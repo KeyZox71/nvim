4338faa4fdafeb715644e20d4277f40bae15e863
Adam JOLY <adjoly@student.42angouleme.fr>
2024-10-30 14:47:56 +0100

    「🔨」 fix(Lua issue): Fuck lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
         build = ':TSUpdate', 
         config = function()
             require('nvim-treesitter.configs').setup {
-                ensure_installed = { "lua" "c" "nix" },
+                ensure_installed = { "lua", "c", "nix" },
                 highlight = {
                     enable = true,
                     use_languagetree = true,
