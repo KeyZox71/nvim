1b67ceeaf3fcbc989b1eccc3e415bcff67a2ef2c
Adam JOLY <adjoly@student.42angouleme.fr>
2024-11-02 14:28:21 +0100

    「🔨」 fix(Wastebin): Change to xdg-open for better compatibility

--- lua/plugins/wastebin.lua
+++ lua/plugins/wastebin.lua
@@ -5,7 +5,7 @@ return {
 		require("wastebin").setup({
 			url = "https://paste.keyzox.me",
 			-- To open on default browser
-			open_cmd = "open",
+			open_cmd = "xdg-open",
 			-- To copy on hyprland
 			-- open_cmd = "wl-copy",
 		})
