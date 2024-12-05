3658da9b75a40b8f675ccd0e89193d2e398c6209
Adam JOLY <adjoly@student.42angouleme.fr>
2024-12-05 11:45:10 +0100

    「🔨」 fix(harpoon): Made harpoon bind usable

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,5 +1,5 @@
 return {
-    "theprimeagen/harpoon",
+	"theprimeagen/harpoon",
 	config = function()
 		local mark = require("harpoon.mark")
 		local ui = require("harpoon.ui")
@@ -8,9 +8,9 @@ return {
 		vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 		vim.keymap.set("n", "<leader>e", ui.toggle_quick_menu)
 
-		vim.keymap.set("n", "<C-r>", function() ui.nav_file(1) end)
-		vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
+		vim.keymap.set("n", "<leader>n", function() ui.nav_file(1) end)
+		vim.keymap.set("n", "<leader>m", function() ui.nav_file(2) end)
 		vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
-		vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+		vim.keymap.set("n", "<C-m>", function() ui.nav_file(4) end)
 	end
 }
